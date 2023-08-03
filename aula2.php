be76835953cf4c4919575a4596b23fd2fb27bc22
pedroricardo14 <pedroricardo1032@gmail.com>
2023-08-03 18:38:34 -0300

    New class

---
+++ aula2.php
@@ -0,0 +1,13 @@
+<?php $nome = 'Pedro'?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esse é o site de <?=$nome?></title>
+</head>
+<body>
+    <h1>Olá! Meu nome é <?=$nome?></h1>
+    <p>Estudando PHP</p>
+</body>
+</html>
