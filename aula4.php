f44f9946200bdbe8a63d8f0aebadc4ebe841098c
pedroricardo14 <pedroricardo1032@gmail.com>
2023-08-05 20:41:47 -0300

    New class: Aula 04

---
+++ aula4.php
@@ -0,0 +1,9 @@
+<?php 
+
+// Primeira forma de criar comentários em PHP.
+echo 'Olá'; echo 'Tudo bem?';
+
+echo 'Comando php antes do comentário'; # Outra forma de criar comentários em PHP.
+echo 'Dois<br>'; /* Este é um
+comentário de múltiplas
+linhas.*/ echo 'Três';
