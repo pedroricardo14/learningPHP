f921261ef0d352a0c72f984363a467f2a1a66ece
pedroricardo14 <pedroricardo1032@gmail.com>
2023-08-09 11:36:33 -0300

    Create aula5.php
    New class in course.

---
+++ aula5.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 100;
+$b = "100a";
+$c = 3.14;
+$d = TRUE; // Verdadeiro
+$d2 = FALSE; // Falso
+
+$mensagem_para_a_humanidade2 = "Olá mundo!";
+
+echo $a;
+echo '<br>';
+echo $b;
+echo '<br>';
+echo $c;
+echo '<br>';
+echo $d;
+echo '<br>';
+echo $d2;
