f1ff432ecba0afb2a0161ca7a089d038b8bd78a6
pedroricardo14 <pedroricardo1032@gmail.com>
2023-09-03 16:42:33 -0300

    New Class: Aula 12 - Estruturas de repeitição while e dowhile.

---
+++ aula12.php
@@ -0,0 +1,19 @@
+<?php
+
+// Aula 12 - Estruturas de repetição while e dowhile
+
+$i = 0;
+/*
+while ($i < 5) {
+    echo 'A variável $i é menor que 5. Valor de $i: '.$i.'<br>';
+    $i ++;
+} 
+
+*/
+
+do {
+    echo 'A variável $i é menor que 5. Valor de $i: '.$i.'<br>';
+    $i++;
+} while ($i < 5);
+
+
